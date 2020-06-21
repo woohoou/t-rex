3754d9afef1dc9ccb5056eb1a68e08221b315cfb
Heber Uriegas <woohoou@users.noreply.github.com>
2020-06-21 16:08:20 -0500

    Updates

---
+++ precache-manifest.da5bcfca975510aaff072425cd198678.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71c3ae40f384360fae81b90d82b9b511",
+    "url": "/t-rex/index.html"
+  },
+  {
+    "revision": "7474f5a13c985ae99b65",
+    "url": "/t-rex/static/css/main.922f5d75.chunk.css"
+  },
+  {
+    "revision": "c8cf6206fbeb111a4ad9",
+    "url": "/t-rex/static/js/2.cf0585f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/t-rex/static/js/2.cf0585f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7474f5a13c985ae99b65",
+    "url": "/t-rex/static/js/main.7e2980a0.chunk.js"
+  },
+  {
+    "revision": "c0626116d3e74e990673",
+    "url": "/t-rex/static/js/runtime-main.efa74af7.js"
+  },
+  {
+    "revision": "f77027a494d7e4cbfc2b88ba57c0106c",
+    "url": "/t-rex/static/media/hit.f77027a4.ogx"
+  },
+  {
+    "revision": "217f1c0d52c4c4302e745639f0ae5458",
+    "url": "/t-rex/static/media/press.217f1c0d.ogx"
+  },
+  {
+    "revision": "705079127aae1ec93bb72b753d68ad81",
+    "url": "/t-rex/static/media/reach.70507912.ogx"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/trex/precache-manifest.8c3e1c4f08293084b415dd606b971bd4.js"
+  "/t-rex/precache-manifest.da5bcfca975510aaff072425cd198678.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trex/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/t-rex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
